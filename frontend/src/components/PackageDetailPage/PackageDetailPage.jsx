99790caeb68a13522db54491278192a4ce6a1c81
sriniketh-reddy <srinikethreddy111@gmail.com>
2025-07-14 20:18:55 +0530

    updation for build warnings

--- frontend/src/components/PackageDetailPage/PackageDetailPage.jsx
+++ frontend/src/components/PackageDetailPage/PackageDetailPage.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { Box, Typography, Container } from '@mui/material';
+import { Typography, Container } from '@mui/material';
 import TourInformation from './TourInformation';
 import TourPlan from './TourPlan';
 import TourLocation from './TourLocation';
 import TourGallery from './TourGallery';
 
 const PackageDetailPage = () => {
-  const { id } = useParams();
 
   return (
     <Container>
